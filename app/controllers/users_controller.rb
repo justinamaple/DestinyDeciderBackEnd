65e17a02d242605e93ad2f819b958be64255af17
Justin Maple <justinamaple@gmail.com>
2019-11-25 14:33:40 -0800

    Set up Signin and Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  def signin
+    @user = User.find_by(email: params[:email])
+    errors = {
+      'errors': {
+        'incorrect': 'email and/or password'
+      }
+    }
+
+    if @user&.authenticate(params[:password])
+      render json: { userId: @user.id }
+    else
+      render json: errors
+    end
+  end
+
+  def signup
+    @user = User.new(
+      email: params[:email],
+      password: params[:password],
+      token: ''
+    )
+
+    if @user.save
+      render json: { userId: @user.id }
+    else
+      render json: { 'errors': @user.errors }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists
-  resources :users
+  post '/signin' => 'users#signin'
+  post '/signup' => 'users#signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
